3ac7beb36b61b59e447d507dbfe8dc6a92efa8b5
Conner Phillip <Conner.cowans@cix.csi.cuny.edu>
2025-02-26 12:50:41 -0500

    Initial commit

---
+++ MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BusinessCardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BusinessCardContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCardContent(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.img_1672_2),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .alpha(0.90f),
+            contentScale = ContentScale.Crop
+        )
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Conner Phillip Cowans", fontSize = 32.sp, color = Color.White)
+            Text(text = "@BlockItScheduler", fontSize = 24.sp, color = Color.White)
+            Text(text = "Conner.cowans@cix.csi.cuny.edu", fontSize = 20.sp, color = Color.White)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BusinessCardPreview() {
+    BusinessCardTheme {
+        BusinessCardContent()
+    }
+}
